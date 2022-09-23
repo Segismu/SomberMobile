9190aae869e99059728e212ab27f03b2bfc9686f
Prometeo <105377032+Segismu@users.noreply.github.com>
2022-09-23 14:16:23 -0300

    How to updated, added touch pause and home menu functions.

---
+++ Assets/Scripts/UI/MobilePauseMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MobilePauseMenu : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+    [SerializeField] GameObject pauseButtonCanvas;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        pauseButtonCanvas.SetActive(false);
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        pauseButtonCanvas.SetActive(true);
+    }
+
+    public void Home(int sceneID)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(sceneID);
+    }
+}
