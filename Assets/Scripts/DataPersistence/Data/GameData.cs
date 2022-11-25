70acd39be46827b4e5a59513962a9d60ad0606a2
PabloCS <titanbenevolo@icloud.com>
2022-11-25 13:13:24 -0300

    Working on saving system.

---
+++ Assets/Scripts/DataPersistence/Data/GameData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class GameData
+{
+    public int deathCount;
+
+    public GameData()
+    {
+        this.deathCount = 0;
+    }
+}
---
+++ Assets/Scripts/DataPersistence/DataPersistenceManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class DataPersistenceManager : MonoBehaviour
+{
+    private GameData gameData;
+    private List<IDataPersistence> dataPersistenceObject;
+
+    public static DataPersistenceManager instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("Found mode than one DPM in scene");
+        }
+
+        instance = this;
+    }
+
+    private void Start()
+    {
+        this.dataPersistenceObject = FindAllDataPersistenceObjects();
+        LoadGame();
+    }
+
+    public void NewGame()
+    {
+        this.gameData = new GameData();
+    }
+
+    public void LoadGame()
+    {
+        if (this.gameData == null)
+        {
+            Debug.Log("No data was found. Initializing data to defaults.");
+            NewGame();
+        }
+
+        foreach (IDataPersistence dataPersistenceObj in dataPersistenceObject)
+        {
+            dataPersistenceObj.LoadData(gameData);
+        }
+
+        Debug.Log("Loaded death count = " + gameData.deathCount);
+    }
+
+    public void SaveGame()
+    {
+        foreach (IDataPersistence dataPersistenceObj in dataPersistenceObject)
+        {
+            dataPersistenceObj.SaveData(ref gameData);
+        }
+
+        Debug.Log("Saved death count = " + gameData.deathCount);
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveGame();
+    }
+
+    private List<IDataPersistence> FindAllDataPersistenceObjects()
+    {
+        IEnumerable<IDataPersistence> dataPersistenceObjects = FindObjectsOfType<MonoBehaviour>().OfType<IDataPersistence>();
+
+        return new List<IDataPersistence>(dataPersistenceObjects);
+    }
+}
